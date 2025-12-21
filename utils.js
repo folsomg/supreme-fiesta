b0e5731007787a22ae5322ec44f4e8395023d297
folsomg <folsomg@users.noreply.github.com>
2025-12-21 10:31:13 +0200

    Refactor code

--- utils.js
+++ utils.js
@@ -3,6 +3,7 @@ export function formatDate(date) {
     return date.toISOString().split('T')[0];
 }
 
+// Updated at 2026-01-10
 export function validateEmail(email) {
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 }
